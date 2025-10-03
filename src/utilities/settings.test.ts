d1c1cf750cf4a684efe83b0ed625b9e3b9a0e738
j-te <34647206+j-te@users.noreply.github.com>
2025-10-03 16:35:49 +0100

    updated unit tests

--- src/utilities/settings.test.ts
+++ src/utilities/settings.test.ts
@@ -1,10 +1,11 @@
-import { assert, describe, expect, it } from "vitest";
+import { assert, describe, expect, expectTypeOf, it } from "vitest";
 import { defaultSettings, loadSettings } from "./settings.ts";
 
 describe("Settings : loadSettings() functions correctly", () => {
   it("loadSettings() correctly returns defaultSettings with bad input #1", () => {
     const settings = loadSettings(undefined);
     expect(settings).deep.equal(defaultSettings);
+    expectTypeOf(settings).toEqualTypeOf(defaultSettings);
   });
 
   it("loadSettings() correctly returns defaultSettings with bad input #2", () => {
@@ -15,6 +16,7 @@ describe("Settings : loadSettings() functions correctly", () => {
   it("loadSettings() correctly returns defaultSettings with bad input #3", () => {
     const settings = loadSettings({ unsupportedValue: [1, 2, 3] });
     expect(settings).deep.equal(defaultSettings);
+    expect(settings.unsupportedValue).toBeUndefined();
   });
 
   it("loadSettings() correctly returns defaultSettings with bad input #4", () => {
@@ -27,8 +29,8 @@ describe("Settings : loadSettings() functions correctly", () => {
       enableDedup: !defaultSettings.enableDedup,
       invalidExample: true,
     });
-    assert(settings !== defaultSettings, "settings should not be equal to defaultSettings.");
-    assert(settings.enableDedup !== defaultSettings.enableDedup, "enableDedup should be different.");
+    expect(settings).not.deep.equal(defaultSettings, "settings should not be equal to defaultSettings");
+    expect(settings.enableDedup).not.toBe(defaultSettings.enableDedup);
   });
 
   it("loadSettings() correctly returns defaultSettings with modifications #2", () => {
@@ -36,14 +38,25 @@ describe("Settings : loadSettings() functions correctly", () => {
       dedupOptions: {
         accessors: !defaultSettings.dedupOptions.accessors,
         meshes: !defaultSettings.dedupOptions.meshes,
+        materials: !defaultSettings.dedupOptions.materials,
       },
       invalidExample: true,
     });
-    assert(settings !== defaultSettings, "settings should not be equal to defaultSettings.");
+    expect(settings).not.deep.equal(defaultSettings, "settings should not be equal to defaultSettings");
     assert(
       settings.dedupOptions.accessors !== defaultSettings.dedupOptions.accessors,
       "accessors should be different."
     );
     assert(settings.dedupOptions.meshes !== defaultSettings.dedupOptions.meshes, "meshes should be different.");
+    assert(settings.dedupOptions.materials !== defaultSettings.dedupOptions.materials, "meshes should be different.");
+  });
+
+  it("loadSettings() correctly returns defaultSettings with modifications #3", () => {
+    const settings = loadSettings({
+      textureCompressionOptions: { format: "jpeg", quality: 1, resize: [512, 512] },
+      invalidExample: true,
+    });
+    expect(settings).not.deep.equal(defaultSettings, "settings should not be equal to defaultSettings");
+    expect(settings.textureCompressionOptions.resize).not.deep.equal(defaultSettings.textureCompressionOptions.resize);
   });
 });
